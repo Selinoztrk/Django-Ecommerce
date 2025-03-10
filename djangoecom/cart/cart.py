5ccdffa03b4a486c450438a07c94bc0216ceec9a
Selin Öztürk <ozturkselin066@gmail.com>
2025-03-10 15:11:55 +0300

    Cart totals

--- djangoecom/cart/cart.py
+++ djangoecom/cart/cart.py
@@ -25,6 +25,26 @@ class Cart():
             self.cart[product_id] = int(product_qty)
         
         self.session.modified = True
+    
+    def cart_total(self):
+        # Get product IDS
+        product_ids = self.cart.keys()
+        # Lookup those keys in our products database model
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantities
+        quantities = self.cart
+        # Start counting at 0
+        total = 0
+
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total
 
     def __len__(self):
         return len(self.cart)
