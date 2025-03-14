35d0f3a22ce8f5143bf4af7ed02046da84c5bb31
Selin Öztürk <ozturkselin066@gmail.com>
2025-03-14 14:46:40 +0300

    Cart persistence pt2

--- djangoecom/cart/cart.py
+++ djangoecom/cart/cart.py
@@ -16,6 +16,28 @@ class Cart():
         # Make sure cart is available on all pages of site
         self.cart = cart
     
+    def db_add(self, product, quantity):
+        product_id = str(product)
+        product_qty = str(quantity)
+
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+        
+        self.session.modified = True
+
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+            # Get the current user profile
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            # Convert {'3':1, '2':4} to {"3":1, "2":4}
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            # Save carty to Profile Model
+            current_user.update(old_cart=str(carty))
+
     def add(self, product, quantity):
         product_id = str(product.id)
         product_qty = str(quantity)
@@ -85,6 +107,16 @@ class Cart():
 
         self.session.modified = True
 
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+            # Get the current user profile
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            # Convert {'3':1, '2':4} to {"3":1, "2":4}
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            # Save carty to Profile Model
+            current_user.update(old_cart=str(carty))
+
         t = self.cart
         return t
     
@@ -95,3 +127,13 @@ class Cart():
             del self.cart[product_id]
         
         self.session.modified = True
+
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+            # Get the current user profile
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+            # Convert {'3':1, '2':4} to {"3":1, "2":4}
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            # Save carty to Profile Model
+            current_user.update(old_cart=str(carty))
--- djangoecom/store/views.py
+++ djangoecom/store/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 from django.db.models import Q
+import json
+from cart.cart import Cart
 
 
 def search(request):
@@ -121,6 +123,22 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+
+            # Do some shopping cart persistent
+            current_user = Profile.objects.get(user__id=request.user.id)
+            # Get their saved cart from Database
+            saved_cart = current_user.old_cart
+            # Convert database string to python dictionary
+            if saved_cart:
+                # Convert to dictionary using JSON
+                converted_cart = json.loads(saved_cart)
+                # Add the loaded cart dictionary to our session
+                cart = Cart(request)
+                # Loop through the cart and add the items from the database
+
+                for key,value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
+
             messages.success(request, ("You have been logged in!"))
             return redirect('home')
         else:
