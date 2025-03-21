6fb875eb6f5a0d989b8a7f5b5ebf9ab1d18f379a
Selin Öztürk <ozturkselin066@gmail.com>
2025-03-21 18:47:45 +0300

    Order items

--- djangoecom/payment/views.py
+++ djangoecom/payment/views.py
@@ -4,6 +4,7 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
 
 
 def payment_success(request):
@@ -91,6 +92,32 @@ def process_order(request):
             create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
 
+            # Add Order Items
+            order_id = create_order.pk # Get the Order ID
+
+            # Get Product Info
+            for product in cart_products():
+                product_id = product.id # Get Product ID
+
+                # Get the Product Price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                
+                # Get quantity
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
+
             messages.success(request, "Order Placed!")
             return redirect('home')
         
@@ -99,6 +126,32 @@ def process_order(request):
             # Create Order
             create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+
+            # Add Order Items
+            order_id = create_order.pk # Get the Order ID
+
+            # Get Product Info
+            for product in cart_products():
+                product_id = product.id # Get Product ID
+
+                # Get the Product Price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                
+                # Get quantity
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+            
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
 
             messages.success(request, "Order Placed!")
             return redirect('home')
